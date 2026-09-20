00e04869a9adbf05e8163ee15db329c180942c9f
Truwarrior1128 <Rob@Lakelandelitepowerwashing.com>
2026-09-20 10:41:34 -0400

    Enable estimate delivery after owner confirms activation and receipt

--- dist/delivery-config.js
+++ dist/delivery-config.js
@@ -1,2 +1,2 @@
-// Keep disabled until Rob activates FormSubmit and confirms a received test.
-window.estimateDelivery = Object.freeze({ enabled: false, endpoint: 'https://formsubmit.co/Rob@Lakelandelitepowerwashing.com' });
+// Rob activated FormSubmit and confirmed receipt of the delivery test on September 20, 2026.
+window.estimateDelivery = Object.freeze({ enabled: true, endpoint: 'https://formsubmit.co/Rob@Lakelandelitepowerwashing.com' });
